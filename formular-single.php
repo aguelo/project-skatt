5ecf015a4156e7af9f3188f48040ec449c05ce15
aguelo <axel.jonsson@live.com>
2015-01-07 14:16:22 +0100

    Fixat formulärdelen. Fungerar ny dynamiskt som den ska.

--- formular-single.php
+++ formular-single.php
@@ -6,57 +6,60 @@
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmnlns="http://www.w3.org/1999/xhtml" xml:lang="sv" lang="sv">
     <head>
-       <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-       <link rel="stylesheet" type="text/css" href="style.css" />
-       <title>Webbskattningsportalen</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        <title>Webbskattningsportalen</title>
     </head>
     <body>
-       <div class="main">
-          <div class="container">
-             <h2>Heading!</h2>
-             <p>
+        <div class="main">
+            <div class="container">
+                <h2>Heading!</h2>
+                <p>
                 <?php
 
                 if (isset($_POST['start'])) {
-                   session_start();
+                    session_start();
+                    $thisFormKey = $_POST['this_form_key'];
+                    //$_SESSION['this_form_key'] = $thisFormKey;
+                    $thisFormIndex = $_POST['this_form_index'];
+                    //$_SESSION['this_form_index'] = $thisFormIndex;
 
+                    echo 'index: ' . $thisFormIndex;
+                    echo '<br />';
+                    echo 'key: ' . $thisFormKey;
+                    echo '<br />';
 
-                   $thisFormKey = $_POST['this_form_key'];
-                   $_SESSION['this_form_key'] = $thisFormKey;
-                   //echo $thisFormKey;
-                   $i = $thisFormKey;
-                   $j = 0;
-                      echo '<h3>' . $_SESSION['form_names'][$i] . ' </h3><br />';
-                      echo '<form action="incoming.php" method="post">';
-                      // Loopa frågor
-                      while ($j < 10) {
-                         getQs($_SESSION['form_keys'][$i]);
+                    $i = $thisFormIndex;
+                    $j = 0;
+                    echo '<h3>' . $_SESSION['form_names'][$i] . ' </h3><br />';
+                    echo '<form action="incoming.php" method="post">';
+                    // Loopa frågor
+                    while ($j < 10) {
 
-                         // Rubrik
-                         echo '<h4>' . $_SESSION['q_string'][$j] . '</h4>';
-                         echo '<br />';
+                        getQs($_SESSION['form_keys'][$i]);
 
-                         //echo '<form action="' . $_SERVER['PHP_SELF'] . '" method="post">';
+                        // Rubrik
+                        echo '<h4>' . $_SESSION['q_string'][$j] . '</h4>';
+                        echo '<br />';
 
-                         // Hämta alternativ
-                         getAlts($_SESSION['form_keys'][$i],$_SESSION['q_key'][$j]);
-                         for ($m=0; $m < 4; $m++) {
+                        // Hämta alternativ
+                        getAlts($_SESSION['form_keys'][$i],$_SESSION['q_key'][$j]);
+                        for ($m=0; $m < 4; $m++) {
                             echo '<input type="radio" name="' . $_SESSION['q_key'][$j] . '" value="' . $_SESSION['alt_key'][$m] . '" ><label for="' . $_SESSION['q_key'][$j] . '">' . $_SESSION['alt_string'][$m] . ' </label>';
-                         }
-
-                         $j++;
-                      }
-                      echo '<input type="submit" name="skicka" value="Skicka skattning">';
-                      echo '</form>';
-
+                        }
+                        $j++;
+                    }
+                    echo '<input type="hidden" name="this_form_index" value="' . ($thisFormIndex) . '">';
+                    echo '<input type="hidden" name="this_form_key" value="' . ($thisFormKey) . '">';
+                    echo '<input type="submit" name="skicka" value="Skicka skattning">';
+                    echo '</form>';
                 }
-
                 ?>
-             </p>
-          </div>
-       </div>
+                </p>
+            </div>
+        </div>
     </body>
-    <footer>
-       <p>Skapad av oss</p>
-    </footer>
+<footer>
+   <p>Skapad av oss</p>
+</footer>
 </html>
--- incoming.php
+++ incoming.php
@@ -6,32 +6,34 @@
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmnlns="http://www.w3.org/1999/xhtml" xml:lang="sv" lang="sv">
     <head>
-       <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-       <link rel="stylesheet" type="text/css" href="style.css" />
-       <title>Webbskattningsportalen</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        <title>Webbskattningsportalen</title>
     </head>
     <body>
-       <div class="main">
-          <div class="container">
-             <h2>Heading!</h2>
-             <p>
+        <div class="main">
+           <div class="container">
+               <h2>Heading!</h2>
+               <p>
                 <?php
-                   if (isset($_POST['skicka'])) {
-                      session_start();
-                      $tfk = $_SESSION['this_form_key'];
-                      echo '<b>f_key: ' . $tfk . '</b><br />';
+                    if (isset($_POST['skicka'])) {
+                        session_start();
+                        $tfk = $_POST['this_form_key'];
+                        $tfi = $_POST['this_form_index'];
+                        echo '<b>f_index: ' . $tfi . '</b><br />';
+                        echo '<b>f_key: ' . $tfk . '</b><br />';
 
-                      for ($j = ($tfk+1); $j < 31; $j++) {
-                         echo $_SESSION['q_key'][$j];
-                         $answer = $_POST[$j];
-                         echo 'alt_key =' . $answer;
-                         echo '<br />';
-                      }
-                   }
+                        for ($j = ($tfi); $j < 31; $j++) {
+                            echo $_SESSION['q_key'][$j];
+                            $answer = $_POST[$j];
+                            echo 'alt_key =' . $answer;
+                            echo '<br />';
+                        }
+                    }
                 ?>
-             </p>
-          </div>
-       </div>
+                </p>
+            </div>
+        </div>
     </body>
     <footer>
        <p>Skapad av oss</p>
