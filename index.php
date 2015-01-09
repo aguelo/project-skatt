ad0300df109ed150f01a379936f96a46c05efbfa
Magnus Ulenius <magnus.ulenius@gmail.com>
2015-01-09 14:43:38 +0100

    Work in progress

--- index.php
+++ index.php
@@ -8,11 +8,28 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <link rel="stylesheet" type="text/css" href="style.css" />
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js"></script>
     <script>
-    jQuery(document).ready(function($){
-        $('#personnummer').autocomplete({source:'suggest.php', minLength:2});
+    $(function() {
+
+        $('#abbrev').val("");
+
+        $("#state").autocomplete({
+            source: "states.php",
+            minLength: 2,
+            select: function(event, ui) {
+                $('#state_id').val(ui.item.p_number);
+                $('#abbrev').val(ui.item.p_lastname);
+            }
+        });
+
+        $("#state_abbrev").autocomplete({
+            source: "states_abbrev.php",
+            minLength: 2
+        });
     });
+
     </script>
     <title>Webbskattningsportalen</title>
 </head>
@@ -78,7 +95,7 @@
                         else {
                                 session_start();
                             ?>
-
+<!--
                             <h2>Skicka ny skattning</h2>
                             <form action="" method="post">
                             <label for="patient_number">Personnummer: </label>
@@ -89,6 +106,32 @@
                             <input name="patient_lastname" type="text">
                             <label for="patient_email">Epostadress: </label>
                             <input name="patient_email" type="text">
+-->
+                        <form action="<?php echo $PHP_SELF;?>" method="post">
+                            <fieldset>
+                                <legend>jQuery UI Autocomplete Example - PHP Backend</legend>
+                                <p>Start typing the name of a state or territory of the United States</p>
+                                <p class="ui-widget">
+                                    <label for="state">State (abbreviation in separate field): </label>
+                                    <input type="text" id="state"  name="state" />
+                                    <input readonly="readonly" type="text" id="abbrev" name="abbrev" maxlength="2" size="2"/></p>
+                                <input type="hidden" id="state_id" name="state_id" />
+                                <p class="ui-widget">
+                                    <label for="state_abbrev">State (replaced with abbreviation): </label>
+                                    <input type="text" id="state_abbrev" name="state_abbrev" /></p>
+                                <p><input type="submit" name="submitBtn" value="Submit" /></p>
+                            </fieldset>
+                        </form>
+                        <?php
+                        if (isset($_POST['submit'])) {
+                            echo "<p>";
+                            while (list($key,$value) = each($_POST)){
+                                echo "<strong>" . $key . "</strong> = ".$value."<br />";
+                            }
+                            echo "</p>";
+                        }
+                        ?>
+
 
                             <?php
                             $sqlForms = "SELECT f_key, f_code, f_name FROM FORM;";
