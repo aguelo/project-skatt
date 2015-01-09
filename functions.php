55802afc0f4a11734e5ad5e9339bac61c9c24bd1
aguelo <axel.jonsson@live.com>
2015-01-09 10:05:44 +0100

    Fixat send-answer.php och flyttat firstForm() ut ur functions.

--- functions.php
+++ functions.php
@@ -205,8 +205,8 @@
     }
 
     // ----- skriv in resultat i databas -----
-    function sendResult($sKey, $res) {
-        $sqlSendResult = "INSERT INTO `RESULT` (`s_key`, `res_value`) VALUES ('$sKey', '$res');";
+    function sendResult($sKey, $strKey, $res) {
+        $sqlSendResult = "INSERT INTO `RESULT` (`s_key`, `str_key`, `res_value`) VALUES ('$sKey', '$strKey', '$res');";
         if ($mysqli = connect_db()) {
             $result = $mysqli->query($sqlSendResult);
             print_r($mysqli->error);
